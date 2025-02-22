77ff9f5f5c6bc34fa7c6f1fb33e24c3c5277119f
gracoba <51455949+gracoba@users.noreply.github.com>
2025-02-22 00:26:08 +0000

    teste

--- formata_despesas.vb
+++ formata_despesas.vb
@@ -1,20 +1,25 @@
 Sub InicializaConfiguracoes()
     Dim i As Integer
-    Dim anos As Variant
+    Dim colunas As Variant
 
-    ' Lista de anos para execução
-    anos = Array("exec_2021", "exec_2022", "exec_2023", "exec_2024")
+    ' Lista de colunas de início
+    colunas = Array(6, 7, 1, 1, 1, 1, 8) ' Exemplo: F, G, A, etc.
 
-    ' Redimensiona o array dinâmico PLAN_EXEC
-    ReDim PLAN_EXEC(LBound(anos) To UBound(anos))
+    ' Redimensiona o array dinâmico COL_INICIO
+    ReDim COL_INICIO(LBound(colunas) To UBound(colunas))
 
     ' Atribui os valores ao array dinâmico
+    For i = LBound(colunas) To UBound(colunas)
+        COL_INICIO(i) = colunas(i)
+    Next i
+
+    ' Inicializa o array PLAN_EXEC
+    Dim anos As Variant
+    anos = Array("exec_2021", "exec_2022", "exec_2023", "exec_2024")
+    ReDim PLAN_EXEC(LBound(anos) To UBound(anos))
     For i = LBound(anos) To UBound(anos)
         PLAN_EXEC(i) = anos(i)
     Next i
-
-    ' Colunas de início para consolidação
-    COL_INICIO = Array(6, 7, 1, 1, 1, 1, 8) ' Exemplo: F, G, A, etc.
 
     ' Configurações de execução
     EXIBE_MENSAGENS = True
